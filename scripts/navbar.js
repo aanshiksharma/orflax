8069995f91ffaa7cc892b30374d2e7fc6ceaa26d
Parth Chaturvedi <parthchaturvedi0204@gmail.com>
2024-09-22 09:54:34 +0530

    changesdoneinindex.htmlandotherfiles

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,1 +1,6 @@
-// 
+const hamburger = document.querySelector('.hamburger');
+const navlist = document.querySelector('.navlist');
+
+hamburger.addEventListener('click', () => {
+  navlist.classList.toggle('active');
+});
