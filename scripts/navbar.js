08c0f04cf548af449d19af553903ff47c5feca52
Aanshik Sharma <aanshik16@gmail.com>
2024-09-22 11:00:53 +0530

    minor fixes

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,1 +1,10 @@
-// 
+const navbar = document.querySelector(".navbar");
+const navbarToggleButton = document.querySelector("header .btn");
+const buttonIcons = document.querySelectorAll("header .btn .bi");
+
+navbarToggleButton.addEventListener("click", () => {
+  navbar.classList.toggle("visible");
+  Array.from(buttonIcons).forEach((icon) => {
+    icon.classList.toggle("display-none");
+  });
+});
